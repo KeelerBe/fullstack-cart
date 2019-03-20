47644abb41b930af89e711608c7b4ad2eb9e2067
Keeler Mendoza <keelerbe@gmail.com>
2019-03-20 16:35:32 +0800

    Modify User schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,7 +5,19 @@ const User = new Schema({
   googleID: String,
   givenName: String,
   familyName: String,
-  email: String
-})
+  email: String,
+  cartProducts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'products'
+  }],
+  cartProductById: {
+    type: Schema.Types.Mixed,
+    default: {}
+  },
+  inventoryProducts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'products'
+  }]
+}, { minimize: false })
 
 module.exports = mongoose.model('users', User)
